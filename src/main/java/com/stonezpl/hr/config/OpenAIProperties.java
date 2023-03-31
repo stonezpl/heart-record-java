8338ead130784de3e02976f318ee7f8ae3583910
张珮磊 <zhangpl@idss-cn.com>
2023-03-31 15:11:15 +0800

    集成jasypt

--- src/main/java/com/stonezpl/hr/config/OpenAIProperties.java
+++ src/main/java/com/stonezpl/hr/config/OpenAIProperties.java
@@ -1,10 +1,6 @@
 package com.stonezpl.hr.config;
 
-import cn.hutool.core.util.CharsetUtil;
-import cn.hutool.crypto.SecureUtil;
-import cn.hutool.crypto.symmetric.SymmetricAlgorithm;
-import cn.hutool.crypto.symmetric.SymmetricCrypto;
-import com.stonezpl.hr.constants.CommonConstants;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -15,45 +11,11 @@ import org.springframework.stereotype.Component;
  */
 @ConfigurationProperties(prefix = "openai")
 @Component
+@Data
 public class OpenAIProperties {
 
     private String apiKey;
     private int maxTokens;
     private String proxyHost;
     private int proxyPort;
-
-    public String getApiKey() {
-        byte[] key =
-                SecureUtil.generateKey(SymmetricAlgorithm.AES.getValue(), CommonConstants.SECURE_KEY.getBytes()).getEncoded();
-        SymmetricCrypto aes = new SymmetricCrypto(SymmetricAlgorithm.AES, key);
-        return aes.decryptStr(this.apiKey, CharsetUtil.CHARSET_UTF_8);
-    }
-
-    public void setApiKey(String apiKey) {
-        this.apiKey = apiKey;
-    }
-
-    public int getMaxTokens() {
-        return maxTokens;
-    }
-
-    public void setMaxTokens(int maxTokens) {
-        this.maxTokens = maxTokens;
-    }
-
-    public String getProxyHost() {
-        return proxyHost;
-    }
-
-    public void setProxyHost(String proxyHost) {
-        this.proxyHost = proxyHost;
-    }
-
-    public int getProxyPort() {
-        return proxyPort;
-    }
-
-    public void setProxyPort(int proxyPort) {
-        this.proxyPort = proxyPort;
-    }
 }
